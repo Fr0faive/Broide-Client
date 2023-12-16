bfa4699f9dfe006c95b7e4c8ac808d453c2665c8
fr0faive <faikar.moch.tajudin@gmail.com>
2023-12-16 18:33:31 +0700

    improvement: responsive navbar & hero

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Logo from "./element/Logo";
 import { ButtonDownload } from "./element/Button";
+import { useState } from "react";
 
 const listNav = [
   {
@@ -18,17 +19,18 @@ const listNav = [
 ];
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
       <nav className="w-full bg-white fixed">
-        <div className="flex justify-between items-center py-6 px-24 mx-24 text-black">
+        <div className="flex justify-between items-center py-6 md:px-3 lg:px-24 mx-16 lg:mx-24 text-black">
           <Link to={"/"} className="flex gap-3 items-center">
             <Logo size="h-10" />
             <span className="text-3xl text-[#2F318B] font-semibold">
               Broide.
             </span>
           </Link>
-          <div className="flex gap-12 font-semibold items-center">
+          <div className="hidden lg:flex gap-12 font-semibold items-center">
             {listNav.map((item, index) => (
               <Link key={index} to={item.path}>
                 {item.name}
@@ -38,7 +40,33 @@ const Navbar = () => {
               Download Katalog
             </ButtonDownload>
           </div>
+          <div className="lg:hidden">
+            {/* Mobile navigation */}
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-2xl focus:outline-none"
+            >
+              ☰
+            </button>
+          </div>
         </div>
+        {/* Mobile menu */}
+        {isMenuOpen && (
+          <div className="lg:hidden w-full flex flex-col justify-center items-center py-4 gap-3">
+            {listNav.map((item, index) => (
+              <Link
+                key={index}
+                to={item.path}
+                className="block py-2 px-4 text-black font-semibold"
+              >
+                {item.name}
+              </Link>
+            ))}
+            <ButtonDownload href="./assets/broide.pdf" type="button">
+              Download Katalog
+            </ButtonDownload>
+          </div>
+        )}
       </nav>
     </>
   );
--- src/component/layout/Hero.jsx
+++ src/component/layout/Hero.jsx
@@ -5,9 +5,9 @@ import Logo from "../element/Logo";
 const Hero = () => {
   return (
     <>
-      <div className="bg-white h-screen w-full">
-        <div className="flex flex-row gap-24 justify-center items-center p-64">
-          <div className="w-2/3">
+      <div className="bg-white h-auto w-full">
+        <div className="flex flex-col-reverse lg:flex-row gap-24 justify-center items-center px-24 py-28 lg:p-64">
+          <div className="w-full lg:w-2/3">
             <div className="flex flex-col gap-5 text-black text-justify">
               <h1 className="text-6xl text-[#2F318B] font-bold">Broide.</h1>
               <p className="text-2xl">
@@ -25,7 +25,7 @@ const Hero = () => {
             </div>
           </div>
           <div className="w-1/3">
-            <Logo size="h-96" />
+            <Logo size="h-30 lg:h-auto" />
           </div>
         </div>
       </div>
