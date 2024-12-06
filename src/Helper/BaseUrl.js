9f04c8841ef42e3bc930fa22b45e1733ad4a4e5a
mkp-kemal <kemal.garut7@gmail.com>
2024-12-06 15:02:27 +0700

    update base url

--- src/Helper/BaseUrl.js
+++ src/Helper/BaseUrl.js
@@ -1,6 +1,7 @@
 const baseURLAPI = (url = '') => {
     url     = url.replace(/^[/]/g,'');
-    const baseURL   = 'http://localhost:3000/api/v1/broide/';
+    // const baseURL   = 'http://localhost:3000/api/v1/broide/';
+    const baseURL   = 'https://broide-be.vercel.app/api/v1/broide/';
     return baseURL + url;
 }
 
